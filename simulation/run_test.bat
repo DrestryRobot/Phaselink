623bf87cb5cac46731b2339b25b6956eef997f78
DrestryRobot <2371478179@qq.com>
2026-08-16 09:45:49 +0800

    del simulation

--- simulation/run_test.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"%~dp0build\release\SimDataPlayer.exe" "C:/Users/23714/Downloads/scan_20260717_141034.csv" "C:/Users/23714/Downloads/scan_20260717_133933.csv" > "%~dp0soundscan_out.txt" 2>&1
-echo EXITCODE=%ERRORLEVEL%
